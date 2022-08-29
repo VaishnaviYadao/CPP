84be32bc435545d879e708b8917951e6f52e8fad
VaishnaviYadao <78222305+VaishnaviYadao@users.noreply.github.com>
2022-08-29 18:48:34 +0530

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int addition(int num1, int num2);
+int main()
+{
+	int num1;
+	int num2;
+	int add;
+	
+	cout<<"Enter first number:";
+	cin>>num1;
+	cout<<"Enter second number:";
+	cin>>num2;
+	
+	add=addition(num1, num2);
+	cout<<"Addition is:"<<add<<endl;
+	return 0;
+}
+int addition(int a,int b)
+{
+	return(a+b);
+}
+
+
